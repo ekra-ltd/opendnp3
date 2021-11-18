5bb527387a507e1dd1c3eb5c9bb27c464ec699ad
po-konstantinov <konstantinov_po@ekra.ru>
2021-11-18 09:34:54 +0300

    fix: Поправлена сборка под Linux

--- cpp/lib/include/opendnp3/outstation/OutstationParams.h
+++ cpp/lib/include/opendnp3/outstation/OutstationParams.h
@@ -35,7 +35,7 @@ namespace opendnp3
 struct OutstationParams
 {
     /// The maximum number of controls the outstation will attempt to process from a single APDU
-    uint32_t maxControlsPerRequest = 4'294'967'295;
+    uint32_t maxControlsPerRequest = 4294967295;
 
     /// How long the outstation will allow an operate to proceed after a prior select
     TimeDuration selectTimeout = TimeDuration::Seconds(10);
